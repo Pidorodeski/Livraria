092361be1a3e8666f501c418c26aba580cc4bb40
Cristian Pidorodeski <cristian.00.bill@gmail.com>
2024-07-08 11:07:08 -0300

    Finalizado autenticação

--- backend/src/middlewares/perfil.js
+++ backend/src/middlewares/perfil.js
@@ -7,9 +7,6 @@ const roles = (listaRoles) => {
         const recursoNome = req.originalUrl.split('?')[0].split('/').slice(1)[0];
         const metodoRequisicao = req.method.toLowerCase();
 
-        console.log(`Nome da Rota: ${recursoNome}`);
-        console.log(`Tipo da rota: ${metodoRequisicao}`);
-
         try {
             const user = await usuario.findById(usuarioId).populate('perfil');
             const roleNome = user.perfil.nomePerfil;
